f18fa016dfaba60212e7a1342fb8205c91015c6c
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-11-16 21:15:04 +0530

    App Bar Design Created : Also Background Colors Added

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,22 @@ class DiceRoll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Dice Roll",
       home: Scaffold(
         backgroundColor: Colors.brown,
+        appBar: AppBar(
+          title: const Text(
+            "Dice Roll",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.brown.shade800,
+        ),
       ),
     );
   }
